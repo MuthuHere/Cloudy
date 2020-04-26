650080b80c08a60c51a5fbe2d6d9025d21eabda1
Muthu <s.muthukhumar@gmail.com>
2020-04-26 08:21:03 +0800

    1. Empty favourite handled

--- lib/ui/citysecection/city_selection_body_widget.dart
+++ lib/ui/citysecection/city_selection_body_widget.dart
@@ -3,6 +3,8 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:weatherapp/data/fav_city.dart';
 import 'package:weatherapp/provider/city_selection_provider.dart';
+import 'package:weatherapp/ui/widgets/app_text.dart';
+import 'package:weatherapp/utils/colors_constant.dart';
 
 class CityBody extends StatelessWidget {
   final CitySelectionProvider provider;
@@ -25,6 +27,12 @@ class CityBody extends StatelessWidget {
         var sortedList = [];
         sortedList =
             searchCityList.where((cities) => cities.isFavourite).toList();
+        if (sortedList.length == 0) {
+          return Center(
+            child: AppText(
+                text: 'No Favourite city selected. Search & add cities',textColor: colorPrimary,),
+          );
+        }
         return ListView.separated(
           itemBuilder: (context, position) {
             FavCity value = sortedList[position];
